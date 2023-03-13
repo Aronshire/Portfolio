60978cb4d3e6a9f920ef9918e640ec3907ffde80
Aronshire <aronshire@gmail.com>
2023-03-13 18:19:21 +0300

    for Kaily

--- config.js
+++ config.js
@@ -1,11 +1,28 @@
+const year = new Date().getFullYear();
+
+const date_ = new Date();
+
+function isAvaible() {
+
+    if(date_.getMonth() >5){
+        return true
+    }
+    if(date_.getMonth() == 5 && date_.getDate() >= 5){
+        return true;
+    }
+}
+
+
 module.exports = {
     discord:{
-        id: "587615441640226837", 
+        id: "587615441640226837",
+        kaiID: "852628541634773063" 
     },
     github:{
         token1: "ghp_glp5HVMRtuOmz6b",
         token2: "sQrU6hpJNdqo10J2IkmuF"
     },
+    kaily: "05/05/" + (isAvaible() ? year+ 1 : year),
     about:{
         description: "Hello, I'm Yusuf, 18 year old developer living in Turkey and technical manager of the PCIFIC Esports team. Recently, I have been working as a volunteer in the positions of director and community manager in esports broadcasts.",
     },
@@ -28,6 +45,7 @@ module.exports = {
     titles:[
         {title: 'Home', url: '/', icon: 'fa-house'},
         {title: 'Projects', url: '/projects' ,icon: 'fa-blog'},
+        {title: 'withKaily', url: '/withkaily', icon: 'fa-heart'},
     ]
 }
 
