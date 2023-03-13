f5f08dd96a6aac316798a542e41012c1c37896fe
Aronshire <aronshire@gmail.com>
2023-03-13 04:32:22 +0300

    first commit

---
+++ config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    discord:{
+        id: "587615441640226837", 
+    },
+    github:{
+        token1: "ghp_glp5HVMRtuOmz6b",
+        token2: "sQrU6hpJNdqo10J2IkmuF"
+    },
+    about:{
+        description: "Hello, I'm Yusuf, 18 year old developer living in Turkey. I am the technical manager of the PCIFIC Esports team.",
+    },
+    social:{
+        github: "https://github.com/Aronshire",
+        instagram: "https://instagram.com/shirreee",
+        twitter: "https://twitter.com/shirreee",
+        discord: "https://discord.com/users/587615441640226837",
+        spotify: "https://open.spotify.com/user/srloru4s9s1vxk65qv02dfd0m",
+    },
+    meta:{
+        title: 'My Personal Website',
+        description: 'My Personal Website',
+        theme: '#02060f',
+        lang: 'en',
+        url: 'https://aronshire.me',
+        image: '',
+    },
+    title: 'Aronshire',
+    titles:[
+        {title: 'Home', url: '/', icon: 'fa-house'},
+        {title: 'Projects', url: '/projects' ,icon: 'fa-blog'},
+    ]
+}
+
